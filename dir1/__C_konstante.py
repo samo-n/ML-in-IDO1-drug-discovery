8236efda78a9a82a1ea5029ce8fb9f3aa9084ab9
samonose <sn7832@student.uni-lj.si>
2024-12-03 01:36:25 +0100

    konstante

---
+++ dir1/__C_konstante.py
@@ -0,0 +1,8 @@
+# insert path to your working dir
+path = '/Users/samonose/PycharmProjects/IDO_inhibitors_ML_predictions/dir1'
+
+# I will save files in the working directory, if you whish to save them elsewhere use this path to do so
+path_files = '/Users/samonose/PycharmProjects/IDO_inhibitors_ML_predictions/dir1'
+
+# insert random seed you will use in the files
+random_seed = 42
